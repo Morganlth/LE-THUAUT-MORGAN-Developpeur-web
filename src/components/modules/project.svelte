8915e3c9bf3fe5a0ef4b7aedeaebf7472c0fbe0b
Morgan <lethuaut.morgan@gmail.com>
2023-07-24 21:51:09 +0200

    commit 24/07/23 21:

--- src/components/modules/project.svelte
+++ src/components/modules/project.svelte
@@ -168,7 +168,7 @@
             canvas_SIZE_DIV_2 = canvas_SIZE / 2
         }
 
-        function project_setEvent() { event.event_addSeveral({ resize: project_resize, scroll: project_scroll }); console.log('set event4') }
+        function project_setEvent() { event.event_addSeveral({ resize: project_resize, scroll: project_scroll }); console.log('set event 5') }
 
         function project_setEventDesktop()
         {
@@ -310,14 +310,13 @@
         // --EVENTS
         async function project_scroll()
         {
-            console.log(event.main_scrollTop >= project_OFFSETTOP - canvas_SIZE)
-            // const NOW = +new Date()
+            const NOW = +new Date()
 
-            // if (NOW > project_LAST + 200)
-            //     event.main_scrollTop >= project_OFFSETTOP - canvas_SIZE
-            //     ? project_start()
-            //     : project_end(),
-            //     project_LAST = NOW
+            if (NOW > project_LAST + 200)
+                event.main_scrollTop >= project_OFFSETTOP - canvas_SIZE
+                ? project_start()
+                : project_end(),
+                project_LAST = NOW
         }
 
         async function project_wheel(deltaY) { if (deltaY > 0 && event.main_scrollTop >= project_OFFSETTOP) cardcontainer_move() }
