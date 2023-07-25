6748228479a8c2babe8fc997729f92c1dc4f1b55
Morgan <lethuaut.morgan@gmail.com>
2023-07-25 09:50:03 +0200

    commit 25/07/23 09:

--- src/components/elements/TicTacToe.svelte
+++ src/components/elements/TicTacToe.svelte
@@ -54,7 +54,7 @@
         tictactoe_AIROUND = false
 
         // --ELEMENT-CELL
-        let cell_STYLE = `
+        let cell_STYLE = (`
             display: flex;
             justify-content: center;
             align-items: center;
@@ -65,7 +65,7 @@
             border: solid ${_colors.sLight} 1px;
             border-top: none;
             border-left: none;
-        `.trim().replaceAll(/((?<=;)\s*[!^\s]|\n)/gm, '')
+        `).trim().replaceAll(/((?<=;)\s*[!^\s]|\n)/gm, '')
 
     // #FUNCTIONS
 
